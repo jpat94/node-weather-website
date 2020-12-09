0587c17d331a43124d4c138aa7da9426038195f5
Justin Patterson <34041914+jpat94@users.noreply.github.com>
2020-12-09 13:24:23 -0600

    added code to let the user see the weather conditions

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find the location provided.', undefined);
         } else {
-            callback(undefined, 'It is currently ' + body.current.temperature + ' degrees outside. It feels like ' + body.current.feelslike + ' degrees.')
+            callback(undefined, 'It is currently ' + body.current.weather_descriptions[0].toLowerCase() + ' and ' + body.current.temperature + ' degrees outside. It feels like ' + body.current.feelslike + ' degrees.')
         }
     });
 };
