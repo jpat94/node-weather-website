8427d1afa6351ebbf1e657ffeb7ec8b1e81d1cd5
Justin Patterson <34041914+jpat94@users.noreply.github.com>
2020-12-09 11:44:06 -0600

    Remove unneeded console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client side JS script loaded');
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#msg-1');
